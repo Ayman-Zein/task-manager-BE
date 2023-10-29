b6d374f578bed461df30cc13ee23533bd65b0c59
Ayman Zein <ayman.zein.elbehairy@gmail.com>
2023-10-29 02:35:52 +0200

    db connection config

--- util/database.js
+++ util/database.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
-const DBConcction = callback => { mongoose.connect(process.env.MONGO_URL).then( ()=> {
-         console.log("DB Connected!!")
-         callback()
-    }).catch(err => {
-        console.log(err);
+const DBConcction = (callback) => {
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("DB Connected!!");
+      callback();
     })
-}
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 exports.DBConcction = DBConcction;
